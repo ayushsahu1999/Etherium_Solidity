bf06c30b72c029304942aea1fe9189ba12ab1e3f
Ayush Sahu <ayushsahu99@gmail.com>
2022-06-07 22:32:49 +0530

    deployed the contract to real rinkeby test network using real accounts, wallet providers and installed truffle.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,6 +4,7 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
 const {abi, evm} = require('../compile');
+const INITIAL_MESSAGE = 'Hi There!'
 
 let accounts;
 let inbox;
@@ -13,7 +14,7 @@ beforeEach(async () => {
 
     inbox = await new web3.eth.Contract(abi).deploy({
         data: evm.bytecode.object,
-        arguments: ['Hi There!'],
+        arguments: [INITIAL_MESSAGE],
     }).send({from: accounts[0], gas: '1000000'});
 });
 
@@ -21,5 +22,16 @@ describe('Inbox', () => {
     it('Deploys a contract', () => {
         // console.log(inbox);
         assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     });
 });
